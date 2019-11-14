f77ec08f21f435018bf26ebaa63b249664a4f6d5
nitaking <10850034+nitaking@users.noreply.github.com>
2019-11-14 20:24:27 +0900

    first commit

---
+++ src/pages/my-history.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './index.css';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <h2>my histroy</h2>
+      <p>年表っぽくかく</p>
+    </div>
+  );
+}
